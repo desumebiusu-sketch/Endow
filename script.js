d8f600fa7cac852c54d7852727ca42cd925fd3b9
desumebiusu-sketch <desumebiusu@gmail.com>
2026-09-16 06:20:22 +0000

    バイト追加機能の修正

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const nextButton = document.getElementById("next");
 const eventDate = document.getElementById("event-date");
 const eventTitle = document.getElementById("event-title");
 const addEventButton = document.getElementById("add-event");
-const addEventButton = document.getElementById("add-event");
+const addWorkButton = document.getElementById("add-work-button");
+
 let currentDate = new Date();
 
 let events = JSON.parse(localStorage.getItem("endow-events")) || {};
